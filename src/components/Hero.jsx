32a3d64472167ad20a03a2067fe26654f184187a
TQ <tianquan95@gmail.com>
2024-02-23 22:14:17 +0800

    initial commit of working proj

---
+++ src/components/Hero.jsx
@@ -0,0 +1,49 @@
+import { useRef, useEffect } from 'react';
+import Typed from 'typed.js';
+
+export default function Hero() {
+  // Create reference to store the DOM element containing the animation
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [
+        // '<span class="bg-gradient-to-r from-green-400 to-slate-400 bg-clip-text pl-4 font-SCPro text-4xl text-transparent">Programmer</span>',
+        // '<span class="bg-gradient-to-r from-pink-500 to-yellow-500 bg-clip-text pl-4 font-PS2P text-4xl text-transparent">Video Gamer</span>',
+        '<span class="pl-4 font-SCPro">Programmer</span>',
+        '<span class="pl-4 font-PS2P text-sm lg:text-[2rem]">Video Gamer</span>',
+      ],
+      typeSpeed: 70,
+      backSpeed: 70,
+      backDelay: 1000,
+      loop: true,
+      loopCount: Infinity,
+      contentType: 'html',
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <section
+      id='hero'
+      className='flex h-screen select-none flex-col items-center justify-center bg-contain bg-center bg-no-repeat'
+      style={{ backgroundImage: 'url(src/assets/rainbow-splatter-70.png)' }}
+    >
+      <h1 className='font-BebasNeue text-[#100C08] lg:text-[7rem] 2xl:text-[8rem]'>
+        Hello! Call me <span className='text-orange-600 '>TQ</span>.
+      </h1>
+      <h2 className='font-BebasNeue text-xl text-[#100C08] lg:text-[3rem]'>
+        I'm a &nbsp; {'{'}
+        <span ref={el} />
+        {'}'}
+      </h2>
+      <p className='relative top-24 text-xs italic text-[#100C08] lg:top-32 lg:text-lg'>
+        Crafting Digital Experiences with Code and Design
+      </p>
+    </section>
+  );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,69 @@
+import { useEffect } from 'react';
+
+export default function NavBar() {
+  useEffect(() => {
+    // Add event listeners to all anchor tags with hash fragments
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').slice(1); // Get the target section ID
+        const targetElement = document.getElementById(targetId); // Find the target section element
+
+        if (targetElement) {
+          // const stickyHeight = document.querySelector('.sticky').offsetHeight; // Get the height of the sticky component
+          const marginTop = parseInt(getComputedStyle(targetElement).marginTop); // Get the margin top of the target section
+          const offset = targetElement.getBoundingClientRect().top - marginTop; // Adjusted offset. Minus away stickyHeight if required.
+
+          window.scrollTo({
+            top: window.scrollY + offset, // Scroll to the adjusted position
+            behavior: 'smooth',
+          });
+        }
+      });
+    });
+  }, []);
+
+  return (
+    <section className='sticky top-4 z-10 flex select-none justify-center'>
+      <div className='w-[75%] rounded-2xl bg-white p-3 shadow-md shadow-slate-400/50 lg:w-[35%] xl:w-[25%] 2xl:w-[21%]'>
+        <ul className='flex justify-center divide-x divide-black font-BebasNeue'>
+          <li>
+            <a href='#hero' className='pr-4 text-black hover:text-orange-500'>
+              <span className='hover:border-b-2 hover:border-orange-500'>
+                Main
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href='#about' className='px-4 text-black hover:text-orange-500'>
+              <span className='hover:border-b-2 hover:border-orange-500 '>
+                About
+              </span>
+            </a>
+          </li>
+          <li>
+            <a
+              href='#projects'
+              className='px-4 text-black hover:text-orange-500'
+            >
+              <span className='hover:border-b-2 hover:border-orange-500'>
+                Works
+              </span>
+            </a>
+          </li>
+          <li>
+            <a
+              href='#contact'
+              className='pl-4 text-black hover:text-orange-500'
+            >
+              <span className='hover:border-b-2 hover:border-orange-500'>
+                Contact
+              </span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
